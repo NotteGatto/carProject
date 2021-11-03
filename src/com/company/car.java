4d4032f802abae053c3bf797255cbff70d499d4c
NotteGatto <ast2605@gmail.com>
2021-11-04 05:27:47 +0600

    basic structure of the car

---
+++ src/com/company/car.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class car {
+    public String color;
+    public String model;
+    public int doors;
+    public String name;
+    public String engineCapacity;
+
+
+}
